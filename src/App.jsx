c8cc875b36499b20f5e939bcfb644e8000daf426
DYLMAZZ <dylanmazzanti@sheridan.edu>
2026-09-23 20:32:58 +0000

    started on React JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+const welcome = {
+    greeting: 'Hey',
+    title: "React",
+};
+
 const title = 'React';
+
+function getTitle(title) {
+    return title;
+}
+
 function App() {
- return (
- <div>
- <h1>Hello React</h1>
- </div>
- );
+  return (
+    <div>
+    <h1>Hello {getTitle('React')}</h1>
+
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+    </div>
+  );
 }
 export default App;
